4bbf529f4c1c8e418356cbea3ca645d24dd531b6
callmesp <995199235@qq.com>
2018-04-23 16:40:35 +0800

    Naive Bayes1.0

--- Bayes/bayes.py
+++ Bayes/bayes.py
@@ -1,4 +1,7 @@
+# -*- coding: UTF-8 -*-
 from numpy import *
+import feedparser
+import re
 
 def loadDataSet():
     postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
@@ -17,6 +20,7 @@ def createVocabList(dataSet):
         vocabSet=vocabSet|set(document)
     return list(vocabSet)
 
+# 将是否出现作为一个特征：词集模型
 def setOfWords2Vec(vocabList,inputSet):
     returnVec=[0]*len(vocabList)
     for word in inputSet:
@@ -26,3 +30,107 @@ def setOfWords2Vec(vocabList,inputSet):
             print('the word: %s is not in my vocabulary'%word)
     return returnVec
 
+# 将出现次数作为特征：词袋模型
+def bagofWords2Vec(vocabList,inputSet):
+    returnVec=[0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)]+=1
+        else:
+            print('the word: %s is not in my vocabulary'%word)
+    return returnVec
+
+def trainNB0(trainMatrix,trainCategory):
+    # 训练样本中的文档数量
+    numTrainDocs=len(trainMatrix)
+    # 词汇表中单词数量
+    numWords=len(trainMatrix[0])
+    # 侮辱性文档占全部文档的比重
+    pAbusive=sum(trainCategory)/float(numTrainDocs)
+
+    p0Num=ones(numWords)
+    p1Num=ones(numWords)
+    p0Denom=2.0
+    p1Denom=2.0
+    for i in range(numTrainDocs):
+        if trainCategory[i]==1:
+            p1Num+=trainMatrix[i]
+            p1Denom+=sum(trainMatrix[i])
+        else:
+            p0Num+=trainMatrix[i]
+            p0Denom+=sum(trainMatrix[i])
+    p1Vect=log(p1Num/p1Denom)
+    p0Vect=log(p0Num/p0Denom)
+
+    return p0Vect,p1Vect,pAbusive
+
+def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
+    p1=sum(vec2Classify*p1Vec)+log(pClass1)
+    p0=sum(vec2Classify*p0Vec)+log(1-pClass1)
+    if p1>p0:
+        return 1
+    else:
+        return 0
+
+def testingNB():
+    listPosts,listClasses=loadDataSet()
+    myVocabList=createVocabList(listPosts)
+    trainMat=[]
+    for postinDoc in listPosts:
+        trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
+    p0V,p1V,pAb=trainNB0(trainMat,listClasses)
+
+    testEntry=['love','my','dalmation']
+    thisDoc=array(setOfWords2Vec(myVocabList,testEntry))
+    print(testEntry,' is classified as :',classifyNB(thisDoc,p0V,p1V,pAb))
+
+    testEntry=['stupid','garbage']
+    thisDoc=array(setOfWords2Vec(myVocabList,testEntry))
+    print(testEntry,' is classified as :',classifyNB(thisDoc,p0V,p1V,pAb))
+
+def textParse(bigString):
+    listofTokens=re.findall(r'\w+',bigString)
+    return [token.lower() for token in listofTokens if len(token)>2]
+
+def spamTest():
+    docList=[];classList=[];fullText=[]
+    # 导入并解析文件
+    for i in range(1,26):
+        # 垃圾邮件
+        wordList=textParse(open('email/spam/%d.txt'%i,'r').read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        # 正常邮件
+        wordList=textParse(open('email/ham/%d.txt'%i,'r').read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    
+    vocabList=createVocabList(docList)
+    trainingSet=list(range(50));testSet=[]
+    # 随机划分成训练集和测试集
+    for i in range(10):
+        randIndex=int(random.uniform(0,len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+
+    trainingMat=[];trainingClasses=[]
+    for docIndex in trainingSet:
+        trainingMat.append(setOfWords2Vec(vocabList,docList[docIndex]))
+        trainingClasses.append(classList[docIndex])
+    
+    p0v,p1v,pSpam=trainNB0(trainingMat,trainingClasses)
+
+    errorCount=0
+
+    for docIndex in testSet:
+        wordVector=setOfWords2Vec(vocabList,docList[docIndex])
+        if classifyNB(wordVector,p0v,p1v,pSpam)!=classList[docIndex]:
+            errorCount+=1
+    print('the error rate is ',float(errorCount)/len(testSet))
+
+if(__name__=='__main__'):
+    ny=feedparser.parse('http://news.163.com/special/00011K6L/rss_newstop.xml') 
+    print(len(ny['entries']))
+   
