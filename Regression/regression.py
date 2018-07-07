9d747098e1ee7ac20b8f465f64eaaeff288a4f0c
callmesp <995199235@qq.com>
2018-07-07 21:28:45 +0800

    regression0.1

---
+++ Regression/regression.py
@@ -0,0 +1,27 @@
+from numpy import *
+
+def loadDataSet(filename):
+    numFeat=len(open(filename).readline().split('\t'))-1
+    dataMat=[]
+    labelMat=[]
+    fr=open(filename)
+    for line in fr.readlines():
+        lineArr=[]
+        curLine=line.strip().split('\t')
+        for i in range(numFeat):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat,labelMat
+
+def standRegres(xArr,yArr):
+    xMat=mat(xArr)
+    yMat=mat(yArr).T
+    xTx=xMat.T*xMat
+    if linalg.det(xTx)==0.0:
+        print('this matrix is singular,can not  do inverse')
+        return
+    ws=xTx.I*(xMat.T*yMat)
+    return ws
+
+
