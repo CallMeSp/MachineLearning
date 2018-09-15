b3b44fc211c266169a98188bea4977b80f6b1c6b
sp <995199235@qq.com>
2018-09-15 20:40:47 +0800

    first

--- DL/TensorFlowTest/test.py
+++ DL/TensorFlowTest/test.py
@@ -1,40 +1,23 @@
 import tensorflow as tf
-from numpy.random import RandomState
 
-batch_size = 8  # 每次学习样本数
+# 创建一个常量 op, 产生一个 1x2 矩阵. 这个 op 被作为一个节点
+# 加到默认图中.
+#
+# 构造器的返回值代表该常量 op 的返回值.
+matrix1 = tf.constant([[3., 3.]])
 
-# 初始化权重
-w1 = tf.Variable(tf.random_normal([2, 3], stddev=1, seed=1))
-w2 = tf.Variable(tf.random_normal([3, 1], stddev=1, seed=1))
+# 创建另外一个常量 op, 产生一个 2x1 矩阵.
+matrix2 = tf.constant([[2.],[2.]])
 
-# 数据集
-x = tf.placeholder(tf.float32, shape=(None, 2), name='x-input')  # 输入
-y_ = tf.placeholder(tf.float32, shape=(None, 1), name='y-input')  # 输出
+# 创建一个矩阵乘法 matmul op , 把 'matrix1' 和 'matrix2' 作为输入.
+# 返回值 'product' 代表矩阵乘法的结果.
+product = tf.matmul(matrix1, matrix2)
 
-# 前向传播
-a = tf.matmul(x, w1)
-y = tf.matmul(a, w2)
+sess=tf.Session()
 
-# 损失函数
-cross_entropy = -tf.reduce_mean(y_ * tf.log(tf.clip_by_value(y, 1e-10, 1.0)))  # todo:看交叉熵损失函数公式
-train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)  # 学习率0.001 优化目标：最小化交叉熵
+result = sess.run(product)
+print (result)
+# ==> [[ 12.]]
 
-# 产生模拟数据集
-rdm = RandomState(1)
-dataset_size = 12800  # 数据集大小
-X = rdm.rand(dataset_size, 2)  # 样本输入
-Y = [[int(x1+x2 < 1)] for(x1, x2) in X]  # 制造的样本结果
-
-# 开始训练
-with tf.Session() as session:
-    session.run(tf.global_variables_initializer())
-    STEPS = 1600  # 训练次数
-    for i in range(STEPS):
-        start = i * batch_size
-        end = min(start+batch_size, dataset_size)
-        # 开始训练
-        session.run(train_step, feed_dict={x: X[start:end], y_: Y[start:end]})
-        # 训练一定时间输出查看交叉熵减小了多少，交叉熵越小误差越小
-        if i % 100 == 0:
-            total_cross_entroy = session.run(cross_entropy, feed_dict={x:X, y_:Y})
-            print(total_cross_entroy)
+# 任务完成, 关闭会话.
+sess.close()
