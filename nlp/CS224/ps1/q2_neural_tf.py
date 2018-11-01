646c5703a72352208085ac4a6cd745d6d31d825a
sp <995199235@qq.com>
2018-11-01 09:25:11 +0800

    time computation

--- nlp/CS224/ps1/q2_neural_tf.py
+++ nlp/CS224/ps1/q2_neural_tf.py
@@ -1,10 +1,9 @@
 import tensorflow as tf
 import numpy as np
-import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 from q1_softmax_tf import softmax
 from q2_sigmoid_tf import sigmoid, sigmoid_grad
 from q2_gradcheck import gradcheck_naive
+import time
 
 
 def forward_backward_prop(data, labels, params, dimensions):
@@ -37,26 +36,33 @@ def forward_backward_prop(data, labels, params, dimensions):
     b2 = np.reshape(params[ofs:ofs + Dy], (1, Dy))
     # ..................................................
     tfW1 = tf.Variable(tf.random_uniform([Dx, H], minval=0, maxval=1))
-    tfb1=tf.Variable(tf.random_uniform([1, H], minval=0, maxval=1))
-    tfW2=tf.Variable(tf.random_uniform([H, Dy], minval=0, maxval=1))
-    tfb2=tf.Variable(tf.random_uniform([1, Dy], minval=0, maxval=1))
+    tfb1 = tf.Variable(tf.random_uniform([1, H], minval=0, maxval=1))
+    tfW2 = tf.Variable(tf.random_uniform([H, Dy], minval=0, maxval=1))
+    tfb2 = tf.Variable(tf.random_uniform([1, Dy], minval=0, maxval=1))
 
     x = tf.placeholder(tf.float32, name="x_input")
     y = tf.placeholder(tf.float32, name="y_input")
 
-    h=tf.nn.sigmoid(tf.add(tf.matmul(x,tfW1),tfb1))
-    y_hat_beforesoftmax=tf.add(tf.matmul(h,tfW2),tfb2)
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=y_hat_beforesoftmax))
-    trainStep=tf.train.GradientDescentOptimizer(0.001).minimize(cost)
+    h = tf.nn.sigmoid(tf.add(tf.matmul(x, tfW1), tfb1))
+    y_hat_beforesoftmax = tf.add(tf.matmul(h, tfW2), tfb2)
+    cost = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(
+            labels=y, logits=y_hat_beforesoftmax))
+    trainStep = tf.train.GradientDescentOptimizer(0.001).minimize(cost)
 
     with tf.Session() as sess:
         init_op = tf.global_variables_initializer()
         sess.run(init_op)
-        for i in range(200000):
-            sess.run(trainStep,feed_dict={x:np.mat(data),y:np.mat(labels)})
-            if i%100 == 0:
-                print('after %s ,loss='%i,sess.run(cost,feed_dict={x:np.mat(data),y:np.mat(labels)}))
-
+        for i in range(20000):
+            sess.run(trainStep, feed_dict={x: np.mat(data), y: np.mat(labels)})
+            if i % 100 == 0:
+                print(
+                    'after %s ,loss=' % i,
+                    sess.run(
+                        cost, feed_dict={
+                            x: np.mat(data),
+                            y: np.mat(labels)
+                        }))
 
 
 def sanity_check():
@@ -82,6 +88,7 @@ def sanity_check():
 
 
 if __name__ == "__main__":
+    startTime = time.time()
     sanity_check()
-    
-    
+    endTime = time.time()
+    print("training took %d seconds" % (endTime - startTime))
